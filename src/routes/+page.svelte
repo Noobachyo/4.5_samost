ab2ec4aff0117a8cd0b5619ad116d519c1556969
Noobachyo <serezha_dubinin_2004@mail.ru>
2024-03-11 19:08:14 +0700

    aaaaaaaaaaaa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,87 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+<title>4.5 Самостоятельное задание</title>
+</svelte:head>
+<div class='container'>
+ <div class='info'>
+  <div class='animate-background'>
+   <div class='container info-container'>
+    <div class='info-text'>
+      <h1>
+      <p class='animate-text1'>Здравствуйте,</p>
+      <p class='text-2'>приветствую вас на своем сайте!</p>
+      </h1>
+    </div>
+        <h2 class='subtitle'>Кликнув по ссылке ниже, вы узнаете, почему играть в волейбол опасно</h2>
+        <div style='color:black' class='scroll-button animate-scroll'tabindex='0'><a href='/about'>Кликайте сюда</a> 
+        <div class='arrow'>→</div>
+        </div>
+    </div>
+  </div>
+</div>
+</div>
+<div class='background-div'></div>
+
+<style>
+ .info {
+ text-align: center;
+ position: relative;
+  background-color: rgb(33, 34, 74);
+ }
+ .info-container {
+ height: 100%;
+ display: flex;
+ flex-direction: column;
+ justify-content: center;
+ text-align: left;
+ align-items: start;
+  background-color: rgb(33, 34, 74);
+ }
+ .container {
+ max-width: 1400px;
+ margin: 190px 0 0 2.5rem;
+ background-color: rgb(33, 34, 74);
+ }
+ .info h1 {
+ font-size: 4rem;
+ margin: 0;
+ }
+ .info h1 p {
+ position: relative;
+ font: inherit;
+ margin: 0;
+ text-align: left;
+ line-height: 130%;
+ width: fit-content;
+ font-weight: 800;
+ letter-spacing: -0.03em;
+ }
+ .info h2 {
+ max-width: 65ch;
+ font-weight: 200;
+ margin-top: 0.50rem;
+ }
+ .background-div {
+ background-color: rgb(33, 34, 74);
+ width: 100%;
+ height: 56vh;
+ margin: -205px auto;
+ }
+ .info .info-text {
+ position: relative;
+ }
+ .scroll-button {
+ display: flex;
+ border: none;
+ outline: none;
+ cursor: pointer;
+ }
+ .scroll-button .arrow {
+ margin-left: 0.50rem;
+ }
+.subtitle{
+  background-color: rgb(33, 34, 74);
+}
+.info-text{
+  background-color: rgb(33, 34, 74);
+}
+</style>
